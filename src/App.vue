fe328bf1edbde846d8e343f08e0dd308b5f2d4e6
Helton Mori <heltonmori@gmail.com>
2023-07-28 00:04:50 -0300

    Header e footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-    </div>
-    <router-view/>
+    <TheHeader />
+    <main id="main"></main>
+    <router-view />
+    <TheFooter />
   </div>
 </template>
 
+<script>
+import TheFooter from "@/components/TheFooter.vue";
+import TheHeader from "@/components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
 <style>
+* {
+  box-sizing: border-box;
+}
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+ul {
+  list-style: none;
+}
+img {
+  max-width: 100%;
+  display: block;
+}
+body {
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #345;
+  background: url(./assets/pattern.svg) repeat top;
+}
+
+a {
+  color: #345;
+  list-style: none;
+  text-decoration: none;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60 90, 0.2);
+  transition: 0.3s all ease;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
 </style>
