a28b5df25af9967d002bf59a2359fbc7c8657bc9
Helton Mori <heltonmori@gmail.com>
2023-07-31 23:36:12 -0300

    Estilo input

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,6 +1,12 @@
 <template>
   <form>
-    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="text"
+      name="busca"
+      id="busca"
+      v-model="busca"
+      placeholder="buscar..."
+    />
     <input
       type="submit"
       id="lupa"
@@ -24,5 +30,36 @@ export default {
   },
 };
 </script>
-<style>
+
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  outline: none;
+  transform: scale(1.1);
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+}
+
+#lupa {
+  width: 62px;
+  height: 58px;
+  background: url(../assets/search.svg) no-repeat center;
+  font-size: 0;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 </style>
