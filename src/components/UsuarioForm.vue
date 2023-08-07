f895be2c298eac877bfe17bb3cfe503c321a9ff0
Helton Mori <heltonmori@gmail.com>
2023-08-06 23:49:45 -0300

    Vuex form

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -25,8 +25,27 @@
 </template>
 
 <script>
+import { mapFields } from "@/helpers.js";
+
 export default {
   name: "UsuarioForm",
+  computed: {
+    ...mapFields({
+      fields: [
+        "nome",
+        "email",
+        "senha",
+        "cep",
+        "rua",
+        "numero",
+        "bairro",
+        "cidade",
+        "estado",
+      ],
+      base: "usuario",
+      mutation: "UPDATE_USUARIO",
+    }),
+  },
 };
 </script>
 
--- src/store.js
+++ src/store.js
@@ -7,6 +7,7 @@ import {
   api
 } from "@/services.js"
 export default new Vuex.Store({
+  strict: true,
   state: {
     login: false,
     usuario: {
@@ -27,7 +28,7 @@ export default new Vuex.Store({
       state.login = payload
     },
     UPDATE_USUARIO(state, payload) {
-      state.usuario = payload
+      state.usuario = Object.assign(state.usuario, payload)
     }
   },
   actions: {
